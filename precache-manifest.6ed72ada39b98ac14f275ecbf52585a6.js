cbe9c7ac04a42f668ead4705fd6bf6841f148748
IhoraDev <Ihor.Lukachuk.Dev@gmail.com>
2021-03-29 19:53:28 +0300

    Updates

---
+++ precache-manifest.6ed72ada39b98ac14f275ecbf52585a6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aa29a750aa51b80656ac9e6f640c418",
+    "url": "/IhoraDev-test_task__for__NerdySoft/index.html"
+  },
+  {
+    "revision": "4a1ee98b78db1c971369",
+    "url": "/IhoraDev-test_task__for__NerdySoft/static/css/2.34640739.chunk.css"
+  },
+  {
+    "revision": "d30c3c1959cd1c12cb10",
+    "url": "/IhoraDev-test_task__for__NerdySoft/static/css/main.b074c8aa.chunk.css"
+  },
+  {
+    "revision": "4a1ee98b78db1c971369",
+    "url": "/IhoraDev-test_task__for__NerdySoft/static/js/2.6be0ef06.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/IhoraDev-test_task__for__NerdySoft/static/js/2.6be0ef06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d30c3c1959cd1c12cb10",
+    "url": "/IhoraDev-test_task__for__NerdySoft/static/js/main.6675af8b.chunk.js"
+  },
+  {
+    "revision": "fe64277a7233872b600e",
+    "url": "/IhoraDev-test_task__for__NerdySoft/static/js/runtime-main.2a0af9cb.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/IhoraDev-test_task__for__NerdySoft/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/IhoraDev-test_task__for__NerdySoft/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/IhoraDev-test_task__for__NerdySoft/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/IhoraDev-test_task__for__NerdySoft/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/IhoraDev-test_task__for__NerdySoft/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
